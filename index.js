9eb7f7bdd44817ac71d2ab4b2c0d5c41e01cc735
mawadasweis19 <mawada.sweis19@gmail.com>
2022-04-01 16:37:42 +0300

    Set the basic endpoint and datasbe in Main point

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Main file
+
+// Get necessery Models
+const express = require('express');
+const conn = require('./models/database');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public', express.static('public'))
+
+// Body Parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
+
+// Set the port
+app.set('port', process.env.PORT || 3030);
+app.listen(app.get('port'));
+console.log(`App listening on port ${app.get('port')}!`);
+
+// Entry endpoint
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/public/view/index.html');
+});
