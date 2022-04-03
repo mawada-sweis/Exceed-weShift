a645dc207433549b99950a0180c31b3cb8c050fb
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2022-04-03 17:35:58 +0300

    Skeleton for Authentication

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 // Get necessery Models
 const express = require('express');
-const conn = require('./models/database');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -18,8 +17,15 @@ app.use(bodyParser.json())
 app.set('port', process.env.PORT || 3030);
 app.listen(app.get('port'));
 console.log(`App listening on port ${app.get('port')}!`);
+console.log(`Visit localhost:${app.get('port')}`);
 
 // Entry endpoint
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/view/index.html');
 });
+
+// Import Index routes.
+var indexRouter = require('./routes/index');
+
+// Using the index routes
+app.use('/', indexRouter);
