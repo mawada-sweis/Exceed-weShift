351e84c9c317a4dba93d4e8e14cde8d368f39379
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2022-04-07 04:33:07 +0300

    Send Email Function

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Promise = require('promise');
+const nodemailer = require('nodemailer');
 
 // Body Parser
 app.use(bodyParser.urlencoded({
@@ -38,7 +39,7 @@ exports.signup = async (req, response) => {
         let saveEmailSql = 'INSERT INTO Account (AccountEmail, City, Name, situation) VALUES (?, ?, ?, ?)';
         con.query(saveEmailSql, [email, city, name, 'Active'], (err, res) => {
             if (err) throw err;
-            
+
             // Insert operation Not Success
             if (res.length > 0) {
                 response.redirect('/');
@@ -64,8 +65,33 @@ access_level = (req, res) => {
     res.send('NOT IMPLEMENTED: access_level');
 };
 
-send_email = (req, res) => {
-    res.send('NOT IMPLEMENTED: send_email');
+send_email = async (email) => {
+    const code = Math.floor(1000 + Math.random() * 9000);
+    
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the Gmail transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "exceed2022web@gmail.com", 
+            pass: "Exceed2022@",
+        },
+    });
+
+    transporter.verify().then(console.log).catch(console.error);
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Exceed- WeShift" <exceed2022web@gmail.com>', // sender address
+        to: email, // receiver
+        subject: "OTP Code", // Subject line
+        text: "Here is your OTD Code: "  + code // plain text body
+    }).then(info => {
+        console.log("Some code to display otp page");
+    }).catch(console.error);
 };
 
 exports.check_otp = (req, res) => {
