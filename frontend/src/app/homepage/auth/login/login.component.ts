33fd6e0a99de771ee3afadeecbc7abaab7783d27
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2022-05-12 01:18:18 +0300

    Signup Backend

--- frontend/src/app/homepage/auth/login/login.component.ts
+++ frontend/src/app/homepage/auth/login/login.component.ts
@@ -9,10 +9,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public LoginForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.LoginForm = this.formBuilder.group({
+      email: ['']
+    });
   }
 
+  login() {
+    let encoding: { [symbol: string]: string } = {};
+    encoding['0'] = ')';
+    encoding['1'] = '!';
+    encoding['2'] = '@';
+    encoding['3'] = '#';
+    encoding['4'] = '$';
+    encoding['5'] = '%';
+    encoding['6'] = '^';
+    encoding['7'] = '&';
+    encoding['8'] = '*';
+    encoding['9'] = '(';
+    this.http
+      .post<any>(
+        'http://localhost:3030/authentication/login',
+        this.LoginForm.value
+      )
+      .subscribe((res: any) => {
+        if (res.status) {
+          let code = res.code;
+          code = '' + code;
+          let codeAfter = '';
+          for (let index = 0; index < code.length; index++) {
+            codeAfter += encoding[code[index]];
+          }
+          this.router.navigate(['otp', codeAfter]);
+        } else {
+          alert('Account is not exist!');
+        }
+      });
+  }
 }
