339ff505bac8e957b48bffa1c4f242c988ad2d5b
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2022-05-12 00:47:31 +0300

    Signup Backend

--- frontend/src/app/homepage/auth/signup/signup.component.ts
+++ frontend/src/app/homepage/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
+  public SignupForm!: FormGroup;
   ngOnInit(): void {
+    this.SignupForm = this.formBuilder.group({
+      fullname: [''],
+      email: [''],
+      city: [''],
+    });
   }
 
+  signUp() {
+    let encoding: { [symbol: string]: string } = {};
+    encoding['0'] = ')';
+    encoding['1'] = '!';
+    encoding['2'] = '@';
+    encoding['3'] = '#';
+    encoding['4'] = '$';
+    encoding['5'] = '%';
+    encoding['6'] = '^';
+    encoding['7'] = '&';
+    encoding['8'] = '*';
+    encoding['9'] = '(';
+
+    this.http
+      .post<any>(
+        'http://localhost:3030/authentication/signup',
+        this.SignupForm.value
+      )
+      .subscribe((res: any) => {
+        if (res.status) {
+          let code = res.code;
+          code = '' + code;
+          let codeAfter = '';
+          for (let index = 0; index < code.length; index++) {
+            codeAfter += encoding[code[index]];
+          }
+          this.router.navigate(['otp', codeAfter]);
+        } else {
+          alert('Account is already exist!');
+        }
+      });
+  }
 }
