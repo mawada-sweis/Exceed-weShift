3dca473b6ca716397db7c079d5b9db8d2fa0899b
Zubaida <sdrzubaida@gmail.com>
2022-05-19 00:35:48 +0300

    queries

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 const Promise = require('promise');
 const nodemailer = require('nodemailer');
 
-// Body Parser
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+// // Body Parser
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json());
 
 exports.dashboard = async(req, res) => {
     console.log('dashboard');
@@ -20,7 +20,7 @@ exports.dashboard = async(req, res) => {
  * CRUD - Read  
  */
 exports.activeCustomers = () => {
-    let getActive = 'SELECT Customer_Active, count(*) FROM Customer GROUP BY Customer_Active ';
+    let getActive = 'SELECT COUNT(*) FROM `Customer` WHERE Customer_Active = "Active" ';
     con.query(getActive, (error, results) => {
         if (error) {
             return console.error(error.message);
@@ -28,4 +28,83 @@ exports.activeCustomers = () => {
         console.log(results);
     })
     console.log('Active customers');
+    return getActive;
+}
+
+
+/**
+ * Count number of requests
+ * CRUD - Read  
+ */
+exports.NumberRequests = () => {
+    let Nreq = 'SELECT COUNT(*) FROM Request';
+    con.query(Nreq, (error, results) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log(results);
+
+    })
+    console.log('Number of Requests');
+}
+
+/**
+ * Display the current shift, its start and end time
+ * CRUD - Read  
+ */
+exports.CurrentShift = () => {
+    let shift = 'SELECT Shift_ID_PK, Shift_Start, Shift_End FROM Shifts GROUP BY Shift_ID_PK ';
+    con.query(shift, (error, results) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log(results);
+    })
+    console.log('Current shift');
+}
+
+/**
+ * Display the Next donation
+ * CRUD - Read  
+ */
+exports.NextDonation = () => {
+    let nextdonation = 'SELECT sum(Next_Donation) as NextD FROM Salary ';
+    con.query(nextdonation, (error, results) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log(results);
+    })
+    console.log('next donation');
+}
+
+/**
+ * Display Total Revenue
+ * Total Revenue = Number of requests * Price 
+ * CRUD - Read  
+ */
+exports.Revenue = () => {
+    let revenue = 'SELECT sum(Request.Reuest_Price) * COUNT(*) FROM Request ';
+    con.query(revenue, (error, results) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log(results);
+    })
+    console.log('Revenue');
+}
+
+/**
+ * Display Cars, drivers, number of requests in the current shift
+ * CRUD - Read  
+ */
+exports.shiftInfo = () => {
+    let shiftInfo = 'SELECT Shift_ID_PK, Driver_ID_FK, Car_ID_FK, Shift_Request_Number FROM Shifts';
+    con.query(shiftInfo, (error, results) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log(results);
+    })
+    console.log('shift Info');
 }
