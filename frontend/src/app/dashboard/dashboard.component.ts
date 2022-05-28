d151f29d6242bde05a42c6861e50504e4e7c6bc1
YaraAbuOdeh <91321242+YaraAbuOdeh@users.noreply.github.com>
2022-05-28 19:38:41 +0300

    click_test

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
+ 
 
   ngOnInit(): void {
   }
+  /*
+  coins(){
+    this.router.navigate(['add-driver'], {relativeTo:this.route});
+  }import { ADDDRIVErComponent } from './../add-driver/add-driver.component';
+notes(){
+    this.router.navigate(['add-driver'], {relativeTo:this.route});
+  }*/
 
 }
